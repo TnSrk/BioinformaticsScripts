20ea18e038ad05b185a4d5f9c98a34587952a902
TnSrk <tanawut.cool@gmail.com>
2016-03-02 15:27:42 +0700

    Third commit

--- select_fastaLen.sh
+++ select_fastaLen.sh
@@ -1,1 +1,2 @@
-more | awk '{if ($0 ~ /^>/) print "#"$0"#";else print $0"#\t"length($0) }' | tr -d '\n' | sed "s/#>/\n>/g" | awk '{if ($2 < 200 ) print $0}' | awk '{print $1}' | tr '#' '\n' | grep -v "^$"
+#usage sh select_fastaLen.sh FASTA_FILE
+more $1 | awk '{if ($0 ~ /^>/) print "#"$0"#";else print $0"#\t"length($0) }' | tr -d '\n' | sed "s/#>/\n>/g" | awk '{if ($2 < 200 ) print $0}' | awk '{print $1}' | tr '#' '\n' | grep -v "^$"
